ce12a65932c9fa223d762c25c98ee6bc184465bf
arbackes <54945366+arbackes@users.noreply.github.com>
2023-05-15 10:58:49 -0300

    Add files via upload

---
+++ Projeto Arvore AVL/ArvoreAVL.h
@@ -0,0 +1,14 @@
+
+typedef struct NO* ArvAVL;
+
+ArvAVL* cria_ArvAVL();
+void libera_ArvAVL(ArvAVL *raiz);
+int insere_ArvAVL(ArvAVL *raiz, int data);
+int remove_ArvAVL(ArvAVL *raiz, int valor);
+int estaVazia_ArvAVL(ArvAVL *raiz);
+int altura_ArvAVL(ArvAVL *raiz);
+int totalNO_ArvAVL(ArvAVL *raiz);
+int consulta_ArvAVL(ArvAVL *raiz, int valor);
+void preOrdem_ArvAVL(ArvAVL *raiz);
+void emOrdem_ArvAVL(ArvAVL *raiz);
+void posOrdem_ArvAVL(ArvAVL *raiz);
